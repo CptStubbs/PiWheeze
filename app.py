8644823a2d5737fad492d65dcc3a128cee8ce5d2
CptStubbs <joshuastubbs@me.com>
2026-01-15 20:31:01 -0800

    Disabled selection for now. Default to terminal mode

--- app.py
+++ app.py
@@ -5,18 +5,11 @@ def main():
     print("*"*50)
     print("Made by CptStubbs")
     print("Welcome to the CO2 Sensor App! \n")
-    print("Type your selection below \n")
-    print("1. Run monitoring in terminal")
-    print("2. Run calibration routine. RUN OUTSIDE. DO NOT RUN OFTEN")
+    print("Running default terminal mode")
+    print("Press Ctrl+C to exit")
     print("*" * 50)
-    selection = int(input("Selection: >>> "))
 
-    if selection == 1:
-        sensor.simple_terminal_mode()
-    elif selection == 2:
-        sensor.calibrate_sensor()
-    else:
-        print("Invalid selection")
+    sensor.simple_terminal_mode()
 
 if __name__ == "__main__":
     main()
