52fcb60cf73042d5c4d6cc1ca659a49eda230ce0
CptStubbs <joshuastubbs@me.com>
2026-01-15 20:17:59 -0800

    Added basic menu options for running

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from co2_sensor.co2_module import Co2Sensor
 
-print("Hello, World!")
+def main():
+    sensor = Co2Sensor()
+    print("*"*50)
+    print("Made by CptStubbs")
+    print("Welcome to the CO2 Sensor App! \n")
+    print("Type your selection below \n")
+    print("1. Run monitoring in terminal")
+    print("2. Run calibration routine. RUN OUTSIDE. DO NOT RUN OFTEN")
+    print("*" * 50)
+    selection = int(input("Selection: >>> "))
+
+    if selection == 1:
+        sensor.simple_terminal_mode()
+    elif selection == 2:
+        sensor.calibrate_sensor()
+    else:
+        print("Invalid selection")
 
 if __name__ == "__main__":
-    co2_sensor = Co2Sensor()
-    co2_sensor.simple_terminal_mode()
+    main()
